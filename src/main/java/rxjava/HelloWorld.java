2db04252d651684d74c2bba7e2f40da160bbd545
张大川 <zdcxp524@sina.com>
2017-04-19 13:30:21 +0800

    添加代码

---
+++ src/main/java/rxjava/HelloWorld.java
@@ -0,0 +1,54 @@
+package rxjava;
+
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
+
+import io.reactivex.BackpressureStrategy;
+import io.reactivex.Flowable;
+import io.reactivex.FlowableEmitter;
+import io.reactivex.FlowableOnSubscribe;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		Flowable<String> flowable = Flowable.create(new FlowableOnSubscribe<String>() {
+
+			@Override
+			public void subscribe(FlowableEmitter<String> e) throws Exception {
+				e.onNext("xxx");
+				e.onNext("hello RxJava 2");
+				e.onComplete();
+			}
+		}, BackpressureStrategy.BUFFER);
+		Subscriber<String> subscriber = new Subscriber<String>() {
+			@Override
+			public void onSubscribe(Subscription s) {
+				System.out.println("onSubscribe");
+				s.request(Long.MAX_VALUE);
+			}
+
+			@Override
+			public void onNext(String s) {
+				try {
+					Thread.sleep(10000);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				System.out.println(s);
+			}
+
+			@Override
+			public void onError(Throwable t) {
+
+			}
+
+			@Override
+			public void onComplete() {
+				System.out.println("onCompxxx");
+			}
+
+		};
+
+		flowable.subscribe(subscriber);
+	}
+}
